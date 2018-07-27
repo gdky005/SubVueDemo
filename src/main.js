98701b7c5015080d5da955c4d46a4d7c8568999e
qingw <qingw@jumei.com>
2018-07-27 13:54:46 +0800

    1. 添加校验器。

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import ElementUI from 'element-ui';
+import Validator from 'vue-validator'
 import 'element-ui/lib/theme-chalk/index.css';
+import App from './App'
+
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+Vue.use(Validator);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  render: h => h(App)
 })
