3a40fc01b4ae688521ed3d878bb3ee5f901daf68
qingw <qingw@jumei.com>
2018-07-27 17:09:08 +0800

    1. 添加登录功能。

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <h1>用户注册和登录内部对话框 调试</h1>
 
     <!--<el-input placeholder="您的用户名或者邮箱" v-model="input3" clearable>-->
-      <!--<template slot="prepend">用户名：</template>-->
+    <!--<template slot="prepend">用户名：</template>-->
     <!--</el-input>-->
 
     <p/>
@@ -37,12 +37,12 @@
           <el-form-item prop="r_password">
             <el-input placeholder="密码" type="password" v-model="registerUserInfo.r_password" clearable>-->
               <template slot="prepend">密　　码：</template>
-              </el-input>
+            </el-input>
           </el-form-item>
 
           <el-form-item prop="r_confirm_password">
             <el-input placeholder="确认密码" type="password" v-model="registerUserInfo.r_confirm_password" clearable>
-            <template slot="prepend">确认密码：</template>
+              <template slot="prepend">确认密码：</template>
             </el-input>
           </el-form-item>
 
@@ -56,51 +56,45 @@
         </el-form>
 
 
-        <!--<div style="padding: 20px auto auto;">-->
-          <!--<div style=" margin: auto;">-->
-            <!--<el-input placeholder="您的用户名或者邮箱" type="text" v-model="r_name" clearable>-->
-              <!--<template slot="prepend">用 户 名：</template>-->
-            <!--</el-input>-->
-          <!--</div>-->
-          <!--<div style=" margin: 30px auto auto;">-->
-            <!--<el-input placeholder="密码" type="password" v-model="r_password" clearable>-->
-              <!--<template slot="prepend">密　　码：</template>-->
-            <!--</el-input>-->
-          <!--</div>-->
-          <!--<div style=" margin: 30px auto auto;">-->
-            <!--<el-input placeholder="确认密码" type="password" v-model="r_confirm_password" clearable>-->
-              <!--<template slot="prepend">确认密码：</template>-->
-            <!--</el-input>-->
-          <!--</div>-->
-          <!--<div style=" margin: 30px auto auto;">-->
-            <!--<el-input placeholder="邮箱地址" type="email" v-model="r_email" clearable>-->
-              <!--<template slot="prepend">邮箱地址：</template>-->
-            <!--</el-input>-->
-          <!--</div>-->
-        <!--</div>-->
-
         <span slot="footer" class="dialog-footer">
           <el-button @click="register(true)">取消</el-button>
-          <el-button style="text-align: center;" type="primary" @click="register(false)" :disabled="registerBtnState">确定</el-button>
+          <el-button style="text-align: center;" type="primary" @click="register(false)"
+                     :disabled="registerBtnState">确定</el-button>
         </span>
-
 
 
       </el-dialog>
 
+      <el-form :model="userInfo" :rules="registerUserInfoRules" ref="userInfo">
+        <el-form-item prop="user_name">
+          <el-input placeholder="您的用户名或者邮箱" type="text" v-model="userInfo.user_name" clearable>
+            <template slot="prepend">用 户 名：</template>
+          </el-input>
+        </el-form-item>
 
-      <div style="padding: 20px auto auto;">
-        <div style=" margin: auto;">
-          <el-input placeholder="您的用户名或者邮箱" v-model="user_name" clearable>
-            <template slot="prepend">用户名：</template>
+        <el-form-item prop="user_password">
+          <el-input placeholder="密码" type="password" v-model="userInfo.user_password" clearable>-->
+            <template slot="prepend">密　　码：</template>
           </el-input>
-        </div>
-        <div style=" margin: 30px auto auto;">
-          <el-input placeholder="密码" type="password" v-model="user_password" clearable>
-            <template slot="prepend">密　码：</template>
-          </el-input>
-        </div>
+        </el-form-item>
+        <!--<el-form-item >-->
+        <!--</el-form-item>-->
+      </el-form>
+      <div style="text-align: center">
+        <el-button type="text" size="mini">如有任何疑问请添加微信：gdky005</el-button>
       </div>
+      <!--<div style="padding: 20px auto auto;">-->
+      <!--<div style=" margin: auto;">-->
+      <!--<el-input placeholder="您的用户名或者邮箱" v-model="user_name" clearable>-->
+      <!--<template slot="prepend">用户名：</template>-->
+      <!--</el-input>-->
+      <!--</div>-->
+      <!--<div style=" margin: 30px auto auto;">-->
+      <!--<el-input placeholder="密码" type="password" v-model="user_password" clearable>-->
+      <!--<template slot="prepend">密　码：</template>-->
+      <!--</el-input>-->
+      <!--</div>-->
+      <!--</div>-->
 
 
       <span slot="footer" class="dialog-footer">
@@ -166,25 +160,36 @@
 
         // 密码校验规则
         registerUserInfoRules: {
+
+          user_name: [
+            {validator: validateUserName, trigger: 'blur'}
+          ],
+          user_password: [
+            {validator: validatePass, trigger: 'blur'}
+          ],
+
           r_name: [
-            { validator: validateUserName, trigger: 'blur' }
+            {validator: validateUserName, trigger: 'blur'}
           ],
           r_password: [
-            { validator: validatePass, trigger: 'blur' }
+            {validator: validatePass, trigger: 'blur'}
           ],
           r_confirm_password: [
-            { validator: validatePass2, trigger: 'blur' }
+            {validator: validatePass2, trigger: 'blur'}
           ],
           r_email: [
-            { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-            { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+            {required: true, message: '请输入邮箱地址', trigger: 'blur'},
+            {type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change']}
           ]
         },
 
 
         //登录信息
-        user_name: '',
-        user_password: '',
+        userInfo: {
+          user_name: '',
+          user_password: '',
+        },
+
 
         //登录框状态
         centerDialogVisible: false,
@@ -197,22 +202,26 @@
     },
     methods: {
       login() {
-        this.centerDialogVisible = false;
-        var log = this.registerUserInfo.user_name + ", ";
-        log += this.user_password;
+
+        var name = this.userInfo.user_name;
+        var password = this.userInfo.user_password;
 
 
-        this.$notify({
-          title: '登录成功',
-          message: log,
-          type: 'success'
+        this.$refs.userInfo.validate((valid) => {
+          if (valid) {
+            this.centerDialogVisible = false;
+            this.innerVisible = false;
+
+            this.$notify({
+              title: '登录成功',
+              message: "当前登录用户是：" + name,
+              type: 'success'
+            });
+          } else {
+            this.$message.error('请先根据页面提示修复错误');
+            return true;
+          }
         });
-
-
-        // this.$message({
-        //   message: log,
-        //   type: 'warning'
-        // });
       },
 
       register(isCancel) {
@@ -232,10 +241,9 @@
         var email = this.registerUserInfo.r_email;
 
         var log = name + ", ";
-        log +=  password + ", ";
-        log +=  confirm_password + ", ";
-        log +=  email + ", ";
-
+        log += password + ", ";
+        log += confirm_password + ", ";
+        log += email + ", ";
 
 
         this.$refs.registerUserInfo.validate((valid) => {
@@ -256,7 +264,7 @@
     },
     watch: {
       registerUserInfo: {  // 这监听对象值的改变 和上面的不一样。
-        handler: function (curVal,oldVal){
+        handler: function (curVal, oldVal) {
           var registerUserInfo = this.registerUserInfo;
 
           if (!!registerUserInfo.r_name &&
